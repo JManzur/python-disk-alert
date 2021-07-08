1bc755c3b26f4a716af3b87b8fb33066345cd242
jmanzur <jmanzurst@gmail.com>
2021-07-08 15:54:12 -0400

    README Edit

--- Windows/README.md
+++ Windows/README.md
@@ -1,3 +1,13 @@
+# Check linux disk capacity and send an email alert
+
+This simple Python script scans a given linux disk (mount point path) and, based on a specified percentage threshold, triggers an email notification that includes a detailed summary of the server and current disk usage. Also a log file is generated every time that the script is run.
+
+## Tested with: 
+
+| Environment | Application | Version  |
+| ----------------- |-----------|---------|
+| Windows 10 21H1 | Python | 3.9.5|
+
 
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 
--- Windows/windows_disk_alert.py
+++ Windows/windows_disk_alert.py
@@ -31,36 +31,46 @@ def disk_var():
     for p in psutil.disk_partitions():
         if p.device == 'C:\\':
             devices = {}
-            disk_free_gb_long = psutil.disk_usage(p.mountpoint).free / 1024 / 1024 / 1024
-            disk_free_gb_short = "{:.2f}".format(disk_free_gb_long)
+            total_gb_long = psutil.disk_usage(p.mountpoint).total / 1024 / 1024 / 1024
+            used_gb_long = psutil.disk_usage(p.mountpoint).used / 1024 / 1024 / 1024
+            free_gb_long = psutil.disk_usage(p.mountpoint).free / 1024 / 1024 / 1024
+            total_gb_short = "{:.2f}".format(total_gb_long)
+            used_gb_short = "{:.2f}".format(used_gb_long)
+            free_gb_short = "{:.2f}".format(free_gb_long)
             disk_used_percent = psutil.disk_usage(p.mountpoint).percent
             devices[p.device] = disk_used_percent
             file_system = p.fstype
             drive = p.device
-            return drive, file_system, disk_used_percent, disk_free_gb_short
+            return drive, file_system, disk_used_percent, total_gb_short, used_gb_short, free_gb_short
 
 def check_disk():
     for p in psutil.disk_partitions():
         if p.device == 'C:\\':
             devices = {}
-            disk_free_gb_long = psutil.disk_usage(p.mountpoint).free / 1024 / 1024 / 1024
-            disk_free_gb_short = "{:.2f}".format(disk_free_gb_long)
+            total_gb_long = psutil.disk_usage(p.mountpoint).total / 1024 / 1024 / 1024
+            used_gb_long = psutil.disk_usage(p.mountpoint).used / 1024 / 1024 / 1024
+            free_gb_long = psutil.disk_usage(p.mountpoint).free / 1024 / 1024 / 1024
+            total_gb_short = "{:.2f}".format(total_gb_long)
+            used_gb_short = "{:.2f}".format(used_gb_long)
+            free_gb_short = "{:.2f}".format(free_gb_long)
             disk_used_percent = psutil.disk_usage(p.mountpoint).percent
             devices[p.device] = disk_used_percent
             file_system = p.fstype
             
             if disk_used_percent > threshold:
                 send_email()
-                logging.warning('Drive: "{0:}", Usage: {1:}%, Free: {2:} GiB, File System: {3:}.'.format(p.device, disk_used_percent, disk_free_gb_short, file_system))
+                logging.warning('Drive: "{0:}", Total Size: {1:} GiB, Used: {2:}, Free: {3:} GiB, Usage Percentage: {4:}%, File System: {5:}.'.format(p.device, total_gb_short, used_gb_short, free_gb_short, disk_used_percent, file_system))
                 logging.warning('NOT OK - Email alert has been sent - Drive "{0:}" has a usage percentage greater than the defined threshold of {1:}%'.format(p.device, threshold))
 
             elif disk_used_percent < threshold:
-                logging.info('Drive: "{0:}", Usage: {1:}%, Free: {2:} GiB, File System: {3:}.'.format(p.device, disk_used_percent, disk_free_gb_short, file_system))
+                logging.info("""\
+                    Drive: "{0:}", Total Size: {1:} GiB, Used: {2:}, Free: {3:} GiB, Usage Percentage: {4:}%, File System: {5:}.
+                    """.format(p.device, total_gb_short, used_gb_short, free_gb_short, disk_used_percent, file_system))
                 logging.info('OK - Drive "{0:}" has a usage percentage less than the defined threshold of {1:}%'.format(p.device, threshold))
 
 # Send Mail funtion
 def send_email():
-    drive, file_system, disk_used_percent, disk_free_gb_short=disk_var()
+    drive, file_system, disk_used_percent, total_gb_short, used_gb_short, free_gb_short=disk_var()
     msg = EmailMessage()
     msg['Subject'] = 'WARNING! Low disk space at ' + hostname
     msg['From'] = EMAIL_ADDRESS
@@ -72,11 +82,13 @@ def send_email():
     Server hostname: {2:}
     IP: {3:}
     
-    Partition: {4:}
-    File System: {5:}
-    Used Percentage: {6:}%
+    Drive: {4:}
+    Total Size: {5:} GiB
+    Total Used: {6:} GiB
     Total Free: {7:} GiB
-    """.format(threshold, now_date, hostname, ip_address, drive, file_system, disk_used_percent, disk_free_gb_short))
+    Used Percentage: {8:}%
+    File System: {9:}
+    """.format(threshold, now_date, hostname, ip_address, drive, total_gb_short, used_gb_short, free_gb_short, disk_used_percent, file_system))
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
         smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
         smtp.send_message(msg)
